d25c45478ebce28e4f3d7e20a3681a8665d7a9b7
Your Name <akhilkumarchandragiri@gmail.com>
2021-12-19 13:31:47 +0530

    day6

---
+++ CouponNumber/CouponNumber/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponNumber
+{
+	class Class1
+	{
+		public static void Coupons(int input)
+		{
+			for (int i = 1; i <= input; i++)
+			{
+				Random random = new Random();
+				int num = random.Next(1000, 999999);
+				Console.WriteLine(i + " : " + num);
+			}
+		}
+	}
+}
+
---
+++ CouponNumber/CouponNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CouponNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Distinct Coupon Codes Do you Want Generate?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(input + " Distinct Coupon Codes are: ");
+
+            Class1.Coupons(input);
+        }
+    }
+}
