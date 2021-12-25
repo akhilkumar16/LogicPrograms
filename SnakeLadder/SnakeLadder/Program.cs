ce11d9dfb4d1766835f985258c551d8761a4caf8
Your Name <akhilkumarchandragiri@gmail.com>
2021-12-25 11:44:26 +0530

    snake and ladder

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+	{
+		public static int currentPosition = 0;
+		static Boolean playerTurn = true;
+		public static int dieRollForPlay(int position, String Player)
+		{
+			const int NO_PLAY = 0;
+			const int LADDER = 1;
+			const int SNAKE = 2;
+			Random random = new Random();
+			int diceNumber = random.Next(1, 7);
+			int playOption = random.Next(0, 3);
+			switch (playOption)
+			{
+				case NO_PLAY:
+					break;
+				case LADDER:
+					position = position + diceNumber;
+					if (position > 100)
+					{
+						position = position - diceNumber;
+					}
+					if (position != 100)
+					{
+						dieRollForPlay(position, Player);
+					}
+					break;
+				case SNAKE:
+					position = position - diceNumber;
+					if (position < 0)
+					{
+						position = 0;
+					}
+					break;
+			}
+			Console.WriteLine("Position of " + Player + "  " + position);
+			return position;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("**********Welcome to Snake And Ladder Game**************");
+			int positionOfPlayer_1 = 0;
+			int positionOfPlayer_2 = 0;
+			int diceRollTotal = 0;
+			while (positionOfPlayer_1 < 100 && positionOfPlayer_2 < 100)
+			{
+				diceRollTotal++;
+				if (playerTurn)
+				{
+					positionOfPlayer_1 = dieRollForPlay(positionOfPlayer_1, " Player1 ");
+				}
+				else
+				{
+					positionOfPlayer_2 = dieRollForPlay(positionOfPlayer_2, " Player2 ");
+				}
+				playerTurn = !playerTurn;
+			}
+			if (positionOfPlayer_1 == 100)
+			{
+				Console.WriteLine("Player 1 wins!! ");
+				Console.WriteLine("Total numbers of time dice rolled for Player 1 is : " + diceRollTotal);
+			}
+			else
+			{
+				Console.WriteLine("Player 2 wins!! ");
+				Console.WriteLine("Total numbers of time dice rolled for Player 2 is : " + diceRollTotal);
+			}
+		}
+	}
+}
+
